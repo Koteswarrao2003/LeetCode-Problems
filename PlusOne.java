7a435126b607e94de551f8d2e8143cbd6b72ea25
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-01 22:51:24 +0530

    Added PlusOne Program

---
+++ PlusOne.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class PlusOne{
+
+    public static int[] plusOne(int []arr){
+
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]<9){
+                arr[i]++;
+                return arr;
+            }
+
+            arr[i]=0;
+        }
+
+        int arr2[]=new int[arr.length+1];
+        arr2[0]=1;
+        return arr2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+
+        if(num==0){
+            int result[]={1};
+            System.out.println(Arrays.toString(result));
+            return;
+        }
+
+        int temp=num;
+        int count=0;
+
+        while(num!=0){
+            num/=10;
+            count++;
+        }
+
+        int arr[]=new int[count];
+
+        for(int i=arr.length-1;i>=0;i--){
+            arr[i]=temp%10;
+            temp/=10;
+        }
+        int arr2[]=plusOne(arr);
+        System.out.println(Arrays.toString(arr2));
+    }
+}
