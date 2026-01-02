aadc5a9f1ecbd35652b4428784dab1025f2cfd80
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-02 20:55:01 +0530

    Added NRepeatedElements

---
+++ NRepeatedElements.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class NRepeatedElements {
+
+    public static int repeatedNTimes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean flag = false;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag) {
+                return nums[i]; 
+            }
+        }
+        return -1; 
+    }
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter size of array: ");
+            int size = sc.nextInt();
+            
+            int[] nums = new int[size];
+            
+            System.out.println("Enter array elements:");
+            for (int i = 0; i < size; i++) {
+                nums[i] = sc.nextInt();
+            }
+            
+            int result = repeatedNTimes(nums);
+            System.out.println("Repeated element is: " + result);
+        }
+    }
+}
