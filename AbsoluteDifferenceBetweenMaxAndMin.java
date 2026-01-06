e0cafcbf3fa8db33927535d9bd1ffe8719874d5b
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-06 21:27:53 +0530

    Updated AbsoluteDifferenceBetweenMaxAndMin

--- AbsoluteDifferenceBetweenMaxAndMin.java
+++ AbsoluteDifferenceBetweenMaxAndMin.java
@@ -20,23 +20,21 @@ public class AbsoluteDifferenceBetweenMaxAndMin {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter array size: ");
-        int n = sc.nextInt();
-
-        int[] nums = new int[n];
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter array size: ");
+            int n = sc.nextInt();
+            
+            int[] nums = new int[n];
+            System.out.println("Enter array elements:");
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+            
+            System.out.print("Enter value of k: ");
+            int k = sc.nextInt();
+            
+            int result = absDifference(nums, k);
+            System.out.println("Absolute Difference: " + result);
         }
-
-        System.out.print("Enter value of k: ");
-        int k = sc.nextInt();
-
-        int result = absDifference(nums, k);
-        System.out.println("Absolute Difference: " + result);
-
-        sc.close();
     }
 }
