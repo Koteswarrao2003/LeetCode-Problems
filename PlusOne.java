fbdf1f7a031d17c501c47caeeabb920c2374db7c
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-01-08 20:03:37 +0530

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -5,7 +5,7 @@ public class PlusOne{
 
         for(int i=arr.length-1;i>=0;i--){
             if(arr[i]<9){
-                arr[i]++;
+                arr[i]++
                 return arr;
             }
 
